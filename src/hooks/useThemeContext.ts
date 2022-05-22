0aa1052b0f3d1d05f83a5350a97635fe6771e671
Brian Caimmi <bcaimmi@gmail.com>
2022-05-22 09:50:26 -0300

    small fix

--- src/hooks/useThemeContext.ts
+++ src/hooks/useThemeContext.ts
@@ -6,9 +6,5 @@ import { ThemeContext, IThemeContext } from '../contexts/theme';
 export const useThemeContext = (): IThemeContext => {
   const context = useContext<IThemeContext>(ThemeContext);
 
-  if (context === undefined) {
-    throw new Error('useThemeContext must be used within the ThemeProvider');
-  }
-
   return context;
 };
